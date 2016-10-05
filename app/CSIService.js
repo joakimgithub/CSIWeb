d772e070ec198fa4ccc7cc61f3582133a90ddd5f
joakimgithub <joakim.gunnarsson@sogeti.com>
2016-10-05 15:11:29 +0200

    first checkin

---
+++ app/CSIService.js
@@ -0,0 +1,15 @@
+//Services. They are objects that can be used by your application 
+//to communicate with a server, an API, or another data source
+
+myApp.service('CSIService', function ($http) {
+
+    this.getAllCustomers = function () {
+        var url = 'http://a01c01101c/CSIService/api/csi_Customer';
+        return $http.get(url).then(function (response) {
+            return response.data;
+        });
+    }
+
+    
+});
+
---
+++ app/csiController.js
@@ -0,0 +1,22 @@
+//When a Controller is attached to the DOM via the ng-controller directive, 
+//Angular will instantiate a new Controller object, using the specified 
+//Controller's constructor function. 
+//A new child scope will be created and made available as an injectable 
+//parameter to the Controller's constructor function as $scope.
+
+myApp.controller('CSIController', function ($scope, CSIService) {
+
+    getAllCustomers();
+    function getAllCustomers() {
+        var servCall = CSIService.getAllCustomers();
+        servCall.then(function (d) {
+            debugger;
+            $scope.customers = d;
+        }, function (error) {
+            $log.error('Oops! Something went wrong while fetching the customer data.')
+        })
+    }
+
+
+})
+
